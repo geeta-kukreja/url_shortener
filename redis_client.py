3b010392ba96ac3f69b735a365085db3a7e14431
geeta-kukreja <geetakukreja133@gmail.com>
2024-10-02 21:25:37 -0400

    kubernetes configuration

--- redis_client.py
+++ redis_client.py
@@ -1,22 +1,24 @@
-import aioredis
+import redis.asyncio as redis
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-redis = None  # Global Redis connection object
+# Global Redis connection object
+redis_client = None
 REDIS_URL = os.getenv('REDIS_URL')
 
 async def get_redis():
-    global redis
-    if redis is None:
-        redis = await aioredis.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
-    return redis
+    global redis_client
+    if redis_client is None:
+        redis_client = redis.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
+    return redis_client
 
 async def close_redis():
-    global redis
-    if redis is not None:
-        await redis.close()
-        redis = None
+    global redis_client
+    if redis_client is not None:
+        await redis_client.close()
+        redis_client = None
 
 async def set_redis(key, value, ttl=3600):
     redis = await get_redis()
